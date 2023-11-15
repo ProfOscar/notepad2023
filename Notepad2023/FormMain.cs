5490c2e7ea19063b8e27a841b544d5ac4bbc60fe
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2023-11-15 12:20:06 +0100

    RichTextBoxEx implementation started

--- Notepad2023/FormMain.cs
+++ Notepad2023/FormMain.cs
@@ -230,32 +230,20 @@ namespace Notepad2023
             }
         }
 
-        private string stringToPrint;
+        private int m_nFirstCharOnPage;
         private void printDocumentMain_BeginPrint(object sender, System.Drawing.Printing.PrintEventArgs e)
         {
-            stringToPrint = rtbMain.Text;
+            m_nFirstCharOnPage = 0;
         }
 
         private void printDocumentMain_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            int charactersOnPage = 0;
-            int linesPerPage = 0;
-
-            // Sets the value of charactersOnPage to the number of characters 
-            // of stringToPrint that will fit within the bounds of the page.
-            e.Graphics.MeasureString(stringToPrint, rtbMain.Font,
-                e.MarginBounds.Size, StringFormat.GenericTypographic,
-                out charactersOnPage, out linesPerPage);
-
-            // Draws the string within the bounds of the page
-            e.Graphics.DrawString(stringToPrint, rtbMain.Font, new SolidBrush(rtbMain.ForeColor),
-                e.MarginBounds, StringFormat.GenericTypographic);
-
-            // Remove the portion of the string that has been printed.
-            stringToPrint = stringToPrint.Substring(charactersOnPage);
-
-            // Check to see if more pages are to be printed.
-            e.HasMorePages = (stringToPrint.Length > 0);
+            m_nFirstCharOnPage = rtbMain.FormatRange(false,
+                                                       e,
+                                                       m_nFirstCharOnPage,
+                                                       rtbMain.TextLength);
+            // check if there are more pages to print
+            e.HasMorePages = m_nFirstCharOnPage < rtbMain.TextLength;
         }
     }
 }
